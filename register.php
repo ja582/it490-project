bc8ab186c77a72aa2b0f5c1acef7ef2f4ecf7398
ja582 <ja582@njit.edu>
2020-02-21 13:17:26 -0500

    Basic login and register page with no functionality. Used for testing webpages on VMs right now.

---
+++ register.php
@@ -0,0 +1,49 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Registration</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+</head>
+<body class="text-center">
+<form class="form-signin" method="POST" action="#">
+    <h1 class="h3 mb-3 font-weight-normal">Registration</h1>
+    <input name="username" type="text" class="form-control" placeholder="Username" required autofocus/>
+    <input name="password" type="password" class="form-control" placeholder="Password" required/>
+    <input name="confirmPassword" type="password" class="form-control" placeholder="Confirm Password" required/>
+    <input type="submit" value="Submit" name="submitButton" id="submitButton"/>
+    <p class="mt-5 mb-3 text-muted">&copy; 2019</p>
+</form>
+</body>
+</html>
