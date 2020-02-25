b53fa3910d63858d605fa4d013dd8997ac31bdc3
mb7 <60150641+mwb25@users.noreply.github.com>
2020-02-25 15:31:05 -0500

    Update RabbitMQClient.php

--- rmq/RabbitMQClient.php
+++ rmq/RabbitMQClient.php
@@ -3,12 +3,14 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+function registerMessage($username, $password){
 $client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
 if(isset($argv[1])){
 	$msg = $argv[1];
 }
 else{
-	$msg = array("message"=>"test message", "type"=>"echo");
+	$msg = array("message"=>"test message", "type"=>"echo"); //goign to have to change this to a different type (
+	
 }
 
 $response = $client->send_request($msg);
@@ -19,3 +21,27 @@ echo "\n\n";
 
 if(isset($argv[0]))
 echo $argv[0] . " END".PHP_EOL;
+}
+
+function loginMessage($username, $password){
+
+$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
+if(isset($argv[1])){
+	$msg = $argv[1];
+}
+else{
+	$msg = array("message"=>"test message", "type"=>"echo"); //going to have to chnage this to different type (type "login")
+	
+}
+
+$response = $client->send_request($msg);
+
+echo "client received response: " . PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+if(isset($argv[0]))
+echo $argv[0] . " END".PHP_EOL;
+}
+
+?>
