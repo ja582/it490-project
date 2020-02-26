29806caf8ead42ea70a96718e1eb78330bc6cd7f
mbell <mwb25@njit.edu>
2020-02-26 16:15:20 -0500

    Created db folder and db connection file for project use

---
+++ db/dbCon.php
@@ -0,0 +1,9 @@
+<?php
+/* Database connection settings */
+$host = '192.168.1.51';
+$user = 'root';
+$pass = 'markit';
+$db = 'it490';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
+?>
