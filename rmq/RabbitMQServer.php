78d67a607e98be9ef49ffe06b3ff4f29db06f84b
mbell <mwb25@njit.edu>
2020-02-28 00:03:37 -0500

    testing phpstorm

--- rmq/RabbitMQServer.php
+++ rmq/RabbitMQServer.php
@@ -3,7 +3,7 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-require_once('dbCon.php');
+require('dbCon.php');
 
 
 function loginMessage($username,$password){
