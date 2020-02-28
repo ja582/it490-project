416b26ba889dacd2a4f88c6bdb93ca7fa92d5dcf
mbell <mwb25@njit.edu>
2020-02-28 00:12:01 -0500

    testing phpstorm

--- rmq/dbCon.php
+++ rmq/dbCon.php
@@ -4,6 +4,10 @@ $host = '192.168.1.51';
 $user = 'root';
 $pass = 'markit';
 $db = 'it490';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+$mysqli = mysqli_connect($host,$user,$pass,$db);
+
+if (mysqli_connect_error()){
+    die("Database Connection failed: ".mysqli_connect_error());
+}
 
 ?>
