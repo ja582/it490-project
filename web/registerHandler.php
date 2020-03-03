2478c550dc933f3e7cce293e54dc03313c36a7ca
mb7 <60150641+mwb25@users.noreply.github.com>
2020-03-03 00:19:28 -0500

    Update registerHandler.php

--- web/registerHandler.php
+++ web/registerHandler.php
@@ -1,5 +1,5 @@
 <?php
-
+//seperate file to space things out and allow pointing to other files. 
 require("/var/www/html/it490-project/rmq/RabbitMQClient.php");
 
 
@@ -9,7 +9,7 @@ $password = $_POST['password'];
 $hash = password_hash($password, PASSWORD_BCRYPT); //hashing. 
 
 
-$rabbitResponse = registerMessage($username,$hash);
+$rabbitResponse = registerMessage($username,$hash); //registerMessage in RabbitMQClient.php. 
 
 if($rabbitResponse==false){
     echo "account already created";
