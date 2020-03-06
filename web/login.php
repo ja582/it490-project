af1730c7eb407b4b1a08dcbce0b1bbaa7d53a029
Joe <ja582@njit.edu>
2020-03-05 20:27:39 -0500

    forgot a ! in the password empty check

--- web/login.php
+++ web/login.php
@@ -6,7 +6,7 @@ if(isset($_POST['submitButton'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
         if($username != "" && $password != "" ){
-            $rabbitResponse = login($username, $password);
+            $rabbitResponse = loginMessage($username, $password);
             if($rabbitResponse==false){
                 echo "login has failed, please try again";
                 //redirect back to login page to try again
