577ca0132117cc6f20b5186391d1d996dd4f21d3
mb7 <60150641+mwb25@users.noreply.github.com>
2020-03-17 16:54:08 -0400

    Create homeSearch.php

---
+++ web/homeSearch.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="homeStyle.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+    <div class="header">
+        <form>
+            <h1> Search for movies and actors</h1>
+            <div class="form-box">
+
+                <input type="text" class="search-field movie" placeholder="movie, actor, ..">
+                <button class="search-btn" type="button">Search</button>
+
+            </div>
+
+
+
+        </form>
+
+
+
+    </div>
+
+
+
+
+    </body>
+
+
+
+
+
+
+
+
+</html>
