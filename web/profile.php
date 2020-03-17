92b7d09e9d7b6370a6909f73381822d3e7cb2fd5
mb7 <60150641+mwb25@users.noreply.github.com>
2020-03-17 15:39:00 -0400

    Create profile.php

---
+++ web/profile.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="profileStyle.css">
+</head>
+
+<body>
+
+<div class="container">
+    <div class="toppane">
+      <h1>Profile Details</h1>
+      <p> <?php echo 'welcome' ?> </p>
+    </div>
+    <div class="leftpane">
+      <h1>Favorite Movies</h1>
+    </div>
+    <div class="middlepane">
+      <h1>Movie Lists<h1>
+
+    </div>
+    <div class="rightpane">
+      <h1>Movie Reviews</h1></div>
+    </div>
+</div>
+
+</body>
+</html>
