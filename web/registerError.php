9311877ef83d3cb5c8dcb7e449120f77c831c8bc
mb7 <60150641+mwb25@users.noreply.github.com>
2020-03-18 17:41:07 -0400

    register error page. option to return to register form

---
+++ web/registerError.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+</head>
+<body>
+
+<p1>
+    Error registering. Account already created. Please try again
+</p1>
+
+<p2>Please return here: <a href="register.php" > Register Form </a></p2>
+
+</body>
+
+</html>
