fcec56345814423bd4f37dd93556224295450bdc
Kharee Owens <keo6@njit.edu>
2020-03-18 22:44:08 -0400

    updating files for app vm

--- web/registerHandler.php
+++ web/registerHandler.php
@@ -1,15 +1,16 @@
+<html>
 <?php
 //seperate file to space things out and allow pointing to other files. 
 require("/var/www/html/it490-project/rmq/RabbitMQClient.php");
-
+echo "random message";
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 $hash = password_hash($password, PASSWORD_BCRYPT); //hashing. 
 
-
 $rabbitResponse = registerMessage($username,$hash); //registerMessage in RabbitMQClient.php. 
+echo 'bye';
 
 if($rabbitResponse==false){
     echo "account already created";
@@ -27,4 +28,4 @@ if($rabbitResponse==false){
 
 
 ?>
-
+</html>
