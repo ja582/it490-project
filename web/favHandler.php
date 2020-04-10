71c64d8e88238d4b5df2b4df2ba1c7613ee67ff3
mb7 <60150641+mwb25@users.noreply.github.com>
2020-04-09 20:22:18 -0400

    handler for fav movie feature

---
+++ web/favHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+if ($_SESSION['logged'] != true){
+    echo "not logged in";
+}
+else{
+    $newUser = $_SESSION['user']['name'];
+}
+
+
+
+require('/var/www/html/it490-project/rmq/RabbitMQClient.php');
+
+$movieText=$_POST['movieText'];
+
+
+movieFavMessage($movieText, $newUser);
+
+
+
+?>
