5371396c797797a9de2b2dd8c3d913701a6c4303
mb7 <60150641+mwb25@users.noreply.github.com>
2020-04-09 20:24:00 -0400

    added function for fav movie feature

--- rmq/RabbitMQClient.php
+++ rmq/RabbitMQClient.php
@@ -24,6 +24,28 @@ function registerMessage($username, $hash){
 	echo $argv[0] . " END".PHP_EOL;
 }
 
+function movieFavMessage($movieText,$newUser){
+	$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
+	if(isset($argv[1])){
+		$msg = $argv[1];
+	}
+	else{
+		$msg = array("message"=>"Fav movie", "type"=>"favMovie", "newUser" => $newUser, "movieText" => $movieText );
+
+	}
+
+	$response = $client->send_request($msg);
+
+	echo "client received response: " . PHP_EOL;
+	return($response);
+	echo "\n\n";
+
+	if(isset($argv[0]))
+		echo $argv[0] . " END".PHP_EOL;
+
+
+}
+
 function loginMessage($username, $password){
 
 	$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
