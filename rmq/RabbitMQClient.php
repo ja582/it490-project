2d4c9a9e3bdf10be073136db1b392165302ad5a1
mb7 <60150641+mwb25@users.noreply.github.com>
2020-04-12 16:49:57 -0400

    added functions for displaying reviews and favs

--- rmq/RabbitMQClient.php
+++ rmq/RabbitMQClient.php
@@ -46,6 +46,51 @@ function movieFavMessage($newUser,$movieText){
 
 }
 
+function displayFavMovie($newUser){
+	$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
+	if(isset($argv[1])){
+		$msg = $argv[1];
+	}
+	else{
+		$msg = array("message"=>"Display Fav", "type"=>"displayFav", "newUser" => $newUser);
+
+	}
+
+	$response = $client->send_request($msg);
+
+	echo "client received response: " . PHP_EOL;
+	return($response);
+	echo "\n\n";
+
+	if(isset($argv[0]))
+		echo $argv[0] . " END".PHP_EOL;
+
+
+}
+
+function displayReviews($newUser){
+	$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
+	if(isset($argv[1])){
+		$msg = $argv[1];
+	}
+	else{
+		$msg = array("message"=>"Display Review", "type"=>"displayReview", "newUser" => $newUser);
+
+	}
+
+	$response = $client->send_request($msg);
+
+	echo "client received response: " . PHP_EOL;
+	return($response);
+	echo "\n\n";
+
+	if(isset($argv[0]))
+		echo $argv[0] . " END".PHP_EOL;
+
+
+}
+
+
 function loginMessage($username, $password){
 
 	$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
