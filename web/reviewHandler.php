d0cab7fa7a4d8790a11b0fe0124214a92d26a06c
mb7 <60150641+mwb25@users.noreply.github.com>
2020-04-14 00:30:04 -0400

    Create reviewHandler.php

---
+++ web/reviewHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+if ($_SESSION['logged'] != true) {
+echo "not logged in";
+} else {
+$newUser = $_SESSION['user']['name'];
+}
+
+
+require('/var/www/html/it490-project/rmq/RabbitMQClient.php');
+
+$review = $_POST['review'];
+
+
+movieReviewMessage($newUser, $review);
+
+
+?>
