6f4e7e9b9949ea1a38b342d86d60011928ff5fa7
mb7 <60150641+mwb25@users.noreply.github.com>
2020-04-23 16:58:14 -0400

    Create api1.sh

---
+++ cloudSetupScripts/api1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt install php
+sudo apt install php-mysqli
+sudo apt install git
+sudo apt install php-mbstring
+sudo apt install composer
+sudo apt install php-bcmath
+composer require php-amqplib/php-amqplib
+
+git clone https://github.com/ja582/it490-project.git
+cd it490-project
+cd rmq
+composer install
