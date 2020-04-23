af055918779bf49ff36f66c214f617a7aefcbc26
mb7 <60150641+mwb25@users.noreply.github.com>
2020-04-23 16:44:57 -0400

    app1 setup

--- cloudSetupScripts/app1.sh
+++ cloudSetupScripts/app1.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+sudo apt-get install apache2
+sudo apt-get install git
+sudo apt-get install php 
+sudo apt-get install php-mbstring
+sudo apt-get install composer
+sudo apt-get install php-bcmath
+composer require php-amqplib/php-amqplib
+sudo apt-get install rabbitmq-server
+
+cd /var/www/html
+git clone https://github.com/ja582/it490-project.git
+cd it490-project
+cd rmq
+composer install
