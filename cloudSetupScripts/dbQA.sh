baf9bb19a39bffbf1be616953d24211cfb60d007
mb7 <60150641+mwb25@users.noreply.github.com>
2020-04-24 15:18:11 -0400

    Create dbQA.sh

---
+++ cloudSetupScripts/dbQA.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install php
+sudo apt install php-mysqli
+sudo apt install php-mbstring
+sudo apt install composer
+sudo apt install php-bcmath
+composer require php-amqplib/php-amqplib
+
+#going to have to composer install in rmq folder after files are transfered in via scp
