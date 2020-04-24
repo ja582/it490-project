ca2321e37104f6cadb62fbac12a5f7738eed8bf7
mb7 <60150641+mwb25@users.noreply.github.com>
2020-04-24 15:22:44 -0400

    Create apiProd.sh

---
+++ cloudSetupScripts/apiProd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#!/bin/bash
+
+sudo apt install php
+sudo apt install curl
+sudo apt install php-mysqli
+sudo apt install php-mbstring
+sudo apt install composer
+sudo apt install php-bcmath
+composer require php-amqplib/php-amqplib
+
+#going to have to composer install in rmq folder after files are transfered in via scp
