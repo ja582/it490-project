9042f6acd58f22985ab4c79f044e50a7878d0dae
mb7 <60150641+mwb25@users.noreply.github.com>
2020-04-25 12:34:46 -0400

    Update rabbitMQSetup1.sh

--- cloudSetupScripts/rabbitMQSetup1.sh
+++ cloudSetupScripts/rabbitMQSetup1.sh
@@ -2,11 +2,11 @@
 
 sudo apt install rabbitmq-server
 
-sudo rabbitmqctl add_user projectDev
+sudo rabbitmqctl add_user projectDev projectDev
 sudo rabbitmqctl set_permissions -p / projectDev ".*" ".*" ".*"
 
-sudo rabbitmqctl add_user projectQA
+sudo rabbitmqctl add_user projectQA projectQA
 sudo rabbitmqctl set_permissions -p / projectQA ".*" ".*" ".*"
 
-sudo rabbitmqctl add_user projectProd
+sudo rabbitmqctl add_user projectProd projectQA
 sudo rabbitmqctl set_permissions -p / projectProd ".*" ".*" ".*"
