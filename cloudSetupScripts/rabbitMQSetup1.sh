18371e3d2edff09394882180d8312c3846ace2e0
mb7 <60150641+mwb25@users.noreply.github.com>
2020-04-25 12:30:58 -0400

    added new user for each lane

--- cloudSetupScripts/rabbitMQSetup1.sh
+++ cloudSetupScripts/rabbitMQSetup1.sh
@@ -2,6 +2,11 @@
 
 sudo apt install rabbitmq-server
 
-sudo rabbitmqctl add_user project
-sudo rabbitmqctl set_permissions -p / project ".*" ".*" ".*"
+sudo rabbitmqctl add_user projectDev
+sudo rabbitmqctl set_permissions -p / projectDev ".*" ".*" ".*"
 
+sudo rabbitmqctl add_user projectQA
+sudo rabbitmqctl set_permissions -p / projectQA ".*" ".*" ".*"
+
+sudo rabbitmqctl add_user projectProd
+sudo rabbitmqctl set_permissions -p / projectProd ".*" ".*" ".*"
