230476012214f91b44e46565af342702fd8179c9
Joe <ja582@njit.edu>
2020-04-27 03:06:21 -0400

    Organized creating tables, and inserts one test user for the server.

--- rmq/config.php
+++ rmq/config.php
@@ -1,8 +1,14 @@
 <?php
-$db = new PDO('sqlite:it490.sqlite');
+$db = new PDO('sqlite:db.sqlite');
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$query = "CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT); CREATE TABLE IF NOT EXISTS movies(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, poster TEXT, imdb_id TEXT); CREATE TABLE IF NOT EXISTS user_movies(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, movie_title TEXT, score INTEGER, FOREIGN KEY(user_id) REFERENCES Users(id)); CREATE TABLE IF NOT EXISTS favorite_movies(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, movie_title TEXT, FOREIGN KEY(user_id) REFERENCES Users(id)); CREATE TABLE IF NOT EXISTS user_reviews(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, review TEXT, FOREIGN KEY(user_id) REFERENCES Users(id))";
+$query =   "CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT); 
+            CREATE TABLE IF NOT EXISTS movies(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, poster TEXT, imdb_id TEXT); 
+            CREATE TABLE IF NOT EXISTS user_movies(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, movie_title TEXT, score INTEGER, FOREIGN KEY(user_id) REFERENCES Users(id)); 
+            CREATE TABLE IF NOT EXISTS favorite_movies(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, movie_title TEXT, FOREIGN KEY(user_id) REFERENCES Users(id)); 
+            CREATE TABLE IF NOT EXISTS user_reviews(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, review TEXT, FOREIGN KEY(user_id) REFERENCES Users(id)); 
+            INSERT INTO Users (username, password) VALUES ("test", "$2y$10$hY13jW2NWn0Oi.iTeJPTo.40uVKzwImgqRCjClKKaRPP4pC7/vyWW")";
 
 $db->exec($query);
+
 ?>
