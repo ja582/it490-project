6ace237ab1cb3a18c01b11201c75858b5645a4fe
ja582 <47122054+ja582@users.noreply.github.com>
2020-04-28 21:05:01 -0400

    Update profile.php

--- web/profile.php
+++ web/profile.php
@@ -3,6 +3,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include_once("blade/header.php");
+require('/var/www/html/it490-project/rmq/RabbitMQClient.php');
 //Displaying movie lists
 $response = displayMovieList($id);
 $responseA = displayFavMovie($id);
