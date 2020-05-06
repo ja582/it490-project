3c7eadd4e564a44be975cf7d8dbe8c0e08253fb0
mb7 <60150641+mwb25@users.noreply.github.com>
2020-05-06 17:15:15 -0400

    Update transferScriptFile.sh

--- deployment/transferScriptFile.sh
+++ deployment/transferScriptFile.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+read -p "source rmq or web subdirectory? " sourceSub
 
 read -p "Enter name of file: " file
 
 read -p "Enter IP of destination: " address
 
-read -p "Enter the destination of file: " destination
+read -p "destination rmq or web subdirectory? " destSub
 
-scp -i /home/ubuntu/.ssh/id_dev_migration $file ubuntu@$address:$destination
 
+scp -i /home/ubuntu/.ssh/id_dev_migration /var/www/html/it490-project/$sourceSub/$file ubuntu@$address:/var/www/html/it490-project/$destSub/
 echo "file successfully transfered"
