5aad6abe60acb0f1df9af37f0058dd8ec652d64a
mb7 <60150641+mwb25@users.noreply.github.com>
2020-05-07 14:00:13 -0400

    added logs to show success failure

--- deployment/transferScriptFile.sh
+++ deployment/transferScriptFile.sh
@@ -8,5 +8,6 @@ read -p "Enter IP of destination: " address
 read -p "destination rmq or web subdirectory? " destSub
 
 
-scp -i /home/ubuntu/.ssh/id_dev_migration /var/www/html/it490-project/$sourceSub/$file ubuntu@$address:/var/www/html/it490-project/$destSub/
-echo "file successfully transfered"
+scp -i /home/ubuntu/.ssh/id_dev_migration /var/www/html/it490-project/$sourceSub/$file ubuntu@$address:/var/www/html/it490-project/$destSub/ && echo "file successfully transferred" || echo "file transfer failed"
+
+
