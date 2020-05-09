1c851578d28759d394adccf5e6e6b60475f3913e
mb7 <60150641+mwb25@users.noreply.github.com>
2020-05-08 20:10:16 -0400

    Create backupScriptApiA.sh

---
+++ deployment/backupScriptApiA.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#This is run on machine A. triggers script in machine B which copies file to directory called "backup"
+
+read -p "Enter IP of destination: " address
+
+
+ssh -i /home/ubuntu/.ssh/id_dev_migration ubuntu@$address "./backupFileApiB.sh"
+
+
+
+./transferScriptApiFile.sh
